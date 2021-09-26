01d7f5b9436c4f330a6e0a0869c6a7832d010f15
Elizabeth Grando <elizzabeth.grando@gmail.com>
2021-09-26 20:09:54 -0300

    area do retangulo

---
+++ src/Retangulo/Principal.java
@@ -0,0 +1,14 @@
+package Retangulo;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+	
+	Retangulo retangulo = new Retangulo(0, 0, 10, 10);
+	
+	System.out.println("A area do retangulo é de: "+retangulo.getArea());
+
+	}
+
+}
